688eab3ad5e187a9ad33842689c5bfe69990523e
Vishnuu011 <vishnurrajeev@gmail.com>
2025-01-10 17:47:02 +0530

    transformation

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from src.loan_prediction.logger import logging
 from src.loan_prediction.exception import CustomException
-from src.loan_prediction.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
-from src.loan_prediction.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig
+from src.loan_prediction.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact, DataTransformationArtifact
+from src.loan_prediction.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig, DataTransformationConfig
 from src.loan_prediction.components.data_ingestion import DataIngestion
 from src.loan_prediction.components.data_validation import DataValidation
+from src.loan_prediction.components.data_tansformation import DataTransformation
 
 
 import sys
@@ -23,5 +24,11 @@ if __name__=='__main__':
         data_validation_artifact=data_validation.initiate_data_validation()
         logging.info("data Validation Completed")
         print(data_validation_artifact)
+        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        logging.info("data Transformation started")
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("data Transformation completed")
     except Exception as e:
         raise CustomException(e, sys)  
--- src/loan_prediction/constant/__init__.py
+++ src/loan_prediction/constant/__init__.py
@@ -13,6 +13,8 @@ FILE_NAME: str = "Loan.csv"
 
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
+TRAIN_REGRESSOR_FILE_NAME: str = "train_reg.csv"
+TEST_REGRESSOR_FILE_NAME: str = "test_reg.csv"
 
 SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 
@@ -42,3 +44,18 @@ DATA_VALIDATION_INVALID_DIR: str = "invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
 PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+
+
+"""
+Data Transformation related constant start with DATA_TRANSFORMATION VAR NAME
+"""
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+
+DATA_TRANSFORMATION_TRAIN_FILE_PATH: str = "train.npy"
+
+DATA_TRANSFORMATION_TEST_FILE_PATH: str = "test.npy"
+
+DATA_TRANSFORMATION_TRAIN_REG_FILE_PATH: str = "train_reg.npy"
+DATA_TRANSFORMATION_TEST_REG_FILE_PATH: str = "test_reg.npy"
