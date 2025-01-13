3066173b2c221daf9e870ba39e63986d165277de
Vishnuu011 <vishnurrajeev@gmail.com>
2025-01-13 23:43:13 +0530

    html updated

--- src/loan_prediction/pipline/prediction_pipeline.py
+++ src/loan_prediction/pipline/prediction_pipeline.py
@@ -39,4 +39,120 @@ class PredictionPipeline:
             preds = model_regression.predict(scaled_data)
             return preds
         except Exception as e:
-            raise CustomException(e, sys)    
+            raise CustomException(e, sys)   
+
+
+
+class CustomData:
+    def __init__(  self,
+        Age: int,
+        AnnualIncome: int,
+        CreditScore:int,
+        EmploymentStatus: str,
+        EducationLevel: str,
+        Experience: int,
+        LoanAmount: int,
+        LoanDuration: int,
+        MaritalStatus:str,
+        NumberOfDependents:int,
+        HomeOwnershipStatus:str,
+        MonthlyDebtPayments:int,
+        CreditCardUtilizationRate:float,
+        NumberOfOpenCreditLines:int,
+        NumberOfCreditInquiries:int,
+        DebtToIncomeRatio:float,
+        BankruptcyHistory:int,
+        LoanPurpose:str,
+        PreviousLoanDefaults:int,
+        PaymentHistory:int,
+        LengthOfCreditHistory:int,
+        SavingsAccountBalance:int,
+        CheckingAccountBalance:int,
+        TotalAssets:int,
+        TotalLiabilities:int,
+        MonthlyIncome:float,
+        UtilityBillsPaymentHistory:float,
+        JobTenure:int,
+        NetWorth:int,
+        BaseInterestRate:float,
+        InterestRate:float,
+        MonthlyLoanPayment:float,
+        TotalDebtToIncomeRatio:float):
+
+        self.Age = Age
+        self.AnnualIncome = AnnualIncome
+        self.CreditScore = CreditScore
+        self.EmploymentStatus =EmploymentStatus
+        self.EducationLevel = EducationLevel
+        self.Experience = Experience
+        self.LoanAmount = LoanAmount
+        self.LoanDuration = LoanDuration
+        self.MaritalStatus =MaritalStatus
+        self.NumberOfDependents = NumberOfDependents
+        self.HomeOwnershipStatus = HomeOwnershipStatus
+        self.MonthlyDebtPayments = MonthlyDebtPayments
+        self.CreditCardUtilizationRate = CreditCardUtilizationRate
+        self.NumberOfOpenCreditLines =NumberOfOpenCreditLines
+        self.NumberOfCreditInquiries = NumberOfCreditInquiries
+        self.DebtToIncomeRatio = DebtToIncomeRatio
+        self.BankruptcyHistory = BankruptcyHistory
+        self.LoanPurpose = LoanPurpose
+        self.PreviousLoanDefaults =PreviousLoanDefaults
+        self.PaymentHistory = PaymentHistory
+        self.LengthOfCreditHistory = LengthOfCreditHistory
+        self.SavingsAccountBalance = SavingsAccountBalance
+        self.CheckingAccountBalance = CheckingAccountBalance
+        self.TotalAssets =TotalAssets
+        self.TotalLiabilities = TotalLiabilities
+        self.MonthlyIncome = MonthlyIncome
+        self.UtilityBillsPaymentHistory = UtilityBillsPaymentHistory
+        self.JobTenure =JobTenure
+        self.NetWorth = NetWorth
+        self.BaseInterestRate = BaseInterestRate
+        self.InterestRate = InterestRate
+        self.MonthlyLoanPayment = MonthlyLoanPayment
+        self.TotalDebtToIncomeRatio =TotalDebtToIncomeRatio
+
+    def get_data_as_data_frame(self):
+        try:
+            custom_data_input_dict = {
+                "gender": [self.Age],
+                "InternetService": [self.AnnualIncome],
+                "Contract": [self.CreditScore],
+                "tenure": [self.EmploymentStatus],
+                "MonthlyCharges": [self.EducationLevel],
+                "gender": [self.Experience],
+                "InternetService": [self.LoanAmount],
+                "Contract": [self.LoanDuration],
+                "tenure": [self.MaritalStatus],
+                "MonthlyCharges": [self.NumberOfDependents],
+                "gender": [self.HomeOwnershipStatus],
+                "InternetService": [self.MonthlyDebtPayments],
+                "Contract": [self.CreditCardUtilizationRate],
+                "tenure": [self.NumberOfOpenCreditLines],
+                "MonthlyCharges": [self.NumberOfCreditInquiries],
+                "gender": [self.DebtToIncomeRatio],
+                "InternetService": [self.BankruptcyHistory],
+                "Contract": [self.LoanPurpose],
+                "tenure": [self.PreviousLoanDefaults],
+                "MonthlyCharges": [self.PaymentHistory],
+                "gender": [self.LengthOfCreditHistory],
+                "InternetService": [self.SavingsAccountBalance],
+                "Contract": [self.CheckingAccountBalance],
+                "tenure": [self.TotalAssets],
+                "MonthlyCharges": [self.TotalLiabilities],
+                "gender": [self.MonthlyIncome],
+                "InternetService": [self.UtilityBillsPaymentHistory],
+                "Contract": [self.JobTenure],
+                "tenure": [self.NetWorth],
+                "MonthlyCharges": [self.BaseInterestRate],
+                "Contract": [self.InterestRate],
+                "tenure": [self.MonthlyLoanPayment],
+                "MonthlyCharges": [self.TotalDebtToIncomeRatio],
+                
+            }
+
+            return pd.DataFrame(custom_data_input_dict, index=[0])
+
+        except Exception as e:
+            raise CustomException(e, sys)         
