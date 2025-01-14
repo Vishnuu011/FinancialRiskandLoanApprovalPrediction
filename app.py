487e530e885c08ede2cb5a11ff7a907704b86af3
Vishnuu011 <vishnurrajeev@gmail.com>
2025-01-14 23:24:16 +0530

    up

--- app.py
+++ app.py
@@ -71,4 +71,5 @@ def predict_datapoint():
             risk_score=result_risk_score,
         )
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 4000))
+    app.run(host="0.0.0.0", port=port)
